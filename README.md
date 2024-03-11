6ce9a322c2f4c5a2593b439ca26801a53908c587
Shadumbra <alex.bulc.96@gmail.com>
2024-03-11 19:44:35 +0100

    documentación realizada

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # proyecto-final-angular-server
-servidor con datos sobre perros
+
+Servidor levantado con nodejs y express que conecta con una API externa para obtener informacion sobre los perros, concretamente una lista de razas y una foto aleatoria de una raza concreta, utilizando la libreria axios.
+
+Este servidor se consume posteriormente en el proyecto final.
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const axios = require("axios");
-const app = express();
-const PORT = process.env.PORT || 3000;
-const cors = require("cors");
+const express = require("express"); // Importar Express para crear el servidor
+const axios = require("axios"); // Importar Axios para realizar solicitudes HTTP
+const app = express(); // Crear una instancia de la aplicación Express
+const PORT = process.env.PORT || 3000; // Definir el puerto del servidor, utilizando el puerto proporcionado por el entorno o el puerto 3000 por defecto
+const cors = require("cors"); // Importar CORS para permitir solicitudes desde otros dominios
 
 // Habilitar CORS
 app.use(cors());
@@ -10,9 +10,12 @@ app.use(cors());
 // Endpoint para obtener la lista de razas de perros
 app.get("/api/razas", async (req, res) => {
   try {
+    // Realizar una solicitud GET a la API externa que proporciona la lista de razas de perros
     const response = await axios.get("https://dog.ceo/api/breeds/list/all");
+    // Enviar la respuesta JSON con los datos obtenidos de la API externa
     res.json(response.data);
   } catch (error) {
+    // Enviar una respuesta de error si ocurre algún problema al obtener las razas de perros
     res.status(500).json({ error: "Error al obtener las razas de perros" });
   }
 });
@@ -20,19 +23,24 @@ app.get("/api/razas", async (req, res) => {
 // Endpoint para obtener una imagen aleatoria de una raza de perro específica
 app.get("/api/imagen/:raza", async (req, res) => {
   try {
+    // Obtener el parámetro de la URL que representa la raza de perro
     const raza = req.params.raza;
+    // Realizar una solicitud GET a la API externa para obtener una imagen aleatoria de la raza de perro específica
     const response = await axios.get(
       `https://dog.ceo/api/breed/${raza}/images/random`
     );
+
+    // Enviar la respuesta JSON con los datos obtenidos de la API externa
     res.json(response.data);
   } catch (error) {
+    // Enviar una respuesta de error si ocurre algún problema al obtener la imagen de la raza de perro
     res
       .status(500)
       .json({ error: "Error al obtener la imagen de la raza de perro" });
   }
 });
 
-// Iniciar el servidor
+// Iniciar el servidor y escuchar las solicitudes en el puerto especificado
 app.listen(PORT, () => {
   console.log(`Servidor Node.js iniciado en el puerto ${PORT}`);
 });
